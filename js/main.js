85fede84c0fedecc3bd3bc03c6416f446dfde8a4
Cyd Segui-Barreto <cmsegui@Cyds-iMac.attlocal.net>
2017-06-20 21:53:06 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = "queen";
-var cardTwo = "king";
 
-cardsInPlay.push("cardOne");
-console.log("User flipped queen")
-
-cardsInPlay.push("cardTwo");
-console.log("User flipped king");
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 }
+
+var flipCard = function (cardId) {
+
+	console.log("User flipped " + cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	cardsInPlay.push(cards[cardId]);
+
+};
+
+flipCard(0);
+flipCard(2);
+
 
 	
